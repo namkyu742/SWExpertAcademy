4f9d3962f9a8e4d852800b5f6b846aa64494c6da
namkyu742 <namkyu742@naver.com>
2022-11-09 17:53:47 +0900

    a

--- SWExpertAcademy/D3/1289_원재의 메모리 복구하기.cpp
+++ SWExpertAcademy/D3/1289_원재의 메모리 복구하기.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -9,7 +10,14 @@ int main()
 	cin >> T;
 
 	for (int tc = 1; tc <= T; tc++) {
-		int result;
+		int result = 0;
+		string input;
+		cin >> input;
+		if (input[0] == '1')result++;
+		for (int i = 1; i < input.length(); i++)
+		{
+			if (input[i - 1] != input[i]) result++;
+		}
 		
 
 		cout << "#" << tc << " " << result << "\n";
@@ -17,3 +25,16 @@ int main()
 
 	return 0;
 }
+
+/*
+#1 49
+#2 1
+#3 19
+#4 23
+#5 15
+#6 19
+#7 6
+#8 27
+#9 30
+#10 8
+*/
